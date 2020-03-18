dfd397c37b1d5c9d2cae42645debd318dcc96ee8
clemencecousin <clemence.cousin@dauphine.eu>
2020-03-18 11:50:16 +0100

    Second Try

--- src/main/java/io/github/clemencecousin/MovableUser.java
+++ src/main/java/io/github/clemencecousin/MovableUser.java
@@ -5,7 +5,12 @@ public class MovableUser {
 	public static double getTotalTime (MovableObject M) {
 		
 		double totaltime = 0.0;
-		
+		totaltime = totaltime + M.getTimeToMove(1);
+		M.brake();
+		totaltime = totaltime + M.getTimeToMove(0.1);
+		M.accelerate();
+		totaltime = totaltime + M.getTimeToMove(1);
+		return totaltime;
 	}
 
 	public static void main(String[] args) {
@@ -13,11 +18,9 @@ public class MovableUser {
 		ConstantSpeedMovable constant = new ConstantSpeedMovable();
 		GenericMovable G1 = new GenericMovable();
 		GenericMovable G2 = new GenericMovable(5.0);
-		
-		G1.accelerate();
-		System.out.println("It takes " + constant.getTimeToMove(1)*60 + " minutes to move of 1 km for the object ConstantSpeedMovable");
-		System.out.println("It takes " + G1.getTimeToMove(1)* 60 + " minutes to move of 1 km for the object GenericMovable");
-		System.out.println("It takes " + G2.getTimeToMove(1)*60 + " minutes to move of 1 km for the object GenericMovable");
+		System.out.println("It takes " + getTotalTime(constant) + " minutes in total for Object1");
+		System.out.println("It takes " + getTotalTime(G1) + " minutes in total for Object2");
+		System.out.println("It takes " + getTotalTime(G2) + " minutes in total for Object3");
 
 	}
 
